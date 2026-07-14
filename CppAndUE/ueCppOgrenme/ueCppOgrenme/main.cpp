b55e3d09bf9e621f02a87e1bc08114aefac13de4
PeQMeZ7 <recepefendi7@gmail.com>
2026-07-14 11:43:28 +0300

    cpp ue ogrenme

---
+++ CppAndUE/ueCppOgrenme/ueCppOgrenme/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ueCppOgrenme
+//
+//  Created by Recep AKKUŞ on 14.07.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
