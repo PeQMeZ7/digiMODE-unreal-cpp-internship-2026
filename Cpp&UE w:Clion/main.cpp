1690f1df886b9c5804cc20ab44c0b3352dacc27b
PeQMeZ7 <recepefendi7@gmail.com>
2026-07-14 15:43:14 +0300

    Basics

--- Cpp&UE w:Clion/main.cpp
+++ Cpp&UE w:Clion/main.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
 using namespace std;
 
-class
+class Animal
+{
+protected:
+    string name;
+
+public:
+    Animal(string n) : name(n)
+    {
+    }
+
+    virtual void MakeSound()
+    {
+        cout << "Ses Çıkarır" << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    Dog(string n) : Animal(n)
+    {
+    }
+
+    void MakeSound() override
+    {
+        cout << name << " " << "Hav Hav" << endl;
+    }
+
+    static void MakeSound2()
+    {
+        cout << "Hav Hav 2" << endl;
+    }
+};
+
 
 int main()
 {
+    Animal* hayvan = new Dog("Karabaş");
+    Dog d("Köpke");
+
+    hayvan->MakeSound();
+
+    d.MakeSound();
 
 
+    Dog::MakeSound2();
 
     return 0;
-
 }
